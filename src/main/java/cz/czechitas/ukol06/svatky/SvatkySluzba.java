b84980cdbdc29410577a4f2388b1f61e65ca016d
Zuzana Kaczmarczyková <kaczmarczykova@gmail.com>
2025-06-15 20:00:15 +0200

    ready

--- src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.time.MonthDay;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SvatkySluzba {
 
@@ -20,8 +21,8 @@ public class SvatkySluzba {
     public SvatkySluzba() throws IOException {
         // TODO načíst seznam svátků ze souboru svatky.json
 
-        // Následující řádek po vlastní implementaci smažete.
-        seznamSvatku = null;
+        seznamSvatku = objectMapper.readValue(cestaKDatum.toFile(), SeznamSvatku.class);
+
     }
 
     public List<String> vyhledatSvatkyDnes() {
@@ -30,13 +31,17 @@ public class SvatkySluzba {
 
     public List<String> vyhledatSvatkyKeDni(MonthDay day) {
         // TODO
+        return seznamSvatku.getSvatky().stream()
+                .filter(s -> s.getDen().equals(day))
+                .map(Svatek::getJmeno)
+                .collect(Collectors.toList());
+
+
+
         // získat seznam svátků
         // převést na Stream
         // pomocí metody filter() vybrat jen ty, které odpovídají zadanému dni (porovnat MonthDay pomocí metodyequals())
         // pomocí metody map() získat z objektu jméno
         // pomocí toList() převést na List
-
-        // Následující řádek po vlastní implementaci smažete.
-        return List.of();
     }
 }
--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -2,12 +2,39 @@ package cz.czechitas.ukol06.svatky;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.time.MonthDay;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SvatkySluzbaTest {
 
+    //Tato metoda testuje pripady, kdy ma svatek jen jedno jmeno
     @Test
-    void vyhledatSvatkyKeDni() {
+    void vyhledatDataSJednimSvatkem() throws IOException {
         //TODO implementovat test metody vyhledatSvatkyKeDni
+
+        SvatkySluzba ss = new SvatkySluzba();
+        var test1Name = ss.vyhledatSvatkyKeDni(MonthDay.of(12, 31));
+        assertTrue(test1Name.contains("Silvestr") && test1Name.size() == 1);
+
+        test1Name = ss.vyhledatSvatkyKeDni(MonthDay.of(12, 30));
+        assertTrue(test1Name.contains("David") && test1Name.size() == 1);
     }
+
+    //Tato metoda testuje pripady, kdy ma svatek vice jmen
+    @Test
+    void vyhledatDataSViceSvatky() throws IOException {
+        SvatkySluzba ss = new SvatkySluzba();
+        var test2Name = ss.vyhledatSvatkyKeDni(MonthDay.of(1, 2));
+        assertTrue(test2Name.contains("Vasil") && test2Name.contains("Karina") && test2Name.size() == 2);
+    }
+
+    @Test
+    void vyhledatDataBezSvatku() throws IOException {
+        SvatkySluzba ss = new SvatkySluzba();
+        var test0Name = ss.vyhledatSvatkyKeDni(MonthDay.of(1, 1));
+        assertTrue(test0Name.size() == 0);
+    }
+
 }
